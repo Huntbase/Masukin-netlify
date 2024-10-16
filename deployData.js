e33ab6df0e5e5b99030cb151c0496336689517f7
Huntbase <152288822+Huntbase@users.noreply.github.com>
2024-10-17 03:13:44 +0700

    Initial commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Benediktus Mikael A'; // TODO: replace this
+
+const studentId = 'FS10715299'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
